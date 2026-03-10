dd932d7d4451828409db5245d42b8db79da10e22
Honeyskz <abrilbedrijchagas@gmail.com>
2026-03-10 10:37:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function infoKeras(){
+alert("Keras es una biblioteca de Python para crear redes neuronales y desarrollar modelos de inteligencia artificial.");
+}
+
+window.onload = function(){
+console.log("Página de biblioteca Keras cargada correctamente");
+}
